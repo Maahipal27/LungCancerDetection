aacb460b9b234e036da88a896829262297048b56
Maahi Pal <maahipal94@gmail.com>
2024-02-21 20:43:28 +0530

    bronchipage

---
+++ src/Components/Bronchipage.jsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+
+const Bronchipage = () => {
+    return ( 
+        <div className="bg-black flex  md:flex-row justify-between items-center md:h-screen  py-5" >
+            <div className=" px-20 flex flex-col justify-evenly">
+            <h1 className="text-green-500 text-3xl md:text-5xl m-8 font-bold ">Lung Cancer and Bronchi</h1>
+        
+            
+            </div>
+            </div>
+
+     );
+}
+ 
+export default Bronchipage;
