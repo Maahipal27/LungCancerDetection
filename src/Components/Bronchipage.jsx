3d8632f698e7395bc0337a1ab66ed8c01c89fcc5
Maahi Pal <maahipal94@gmail.com>
2024-02-21 20:54:11 +0530

    commit

--- src/Components/Bronchipage.jsx
+++ src/Components/Bronchipage.jsx
@@ -6,7 +6,7 @@ const Bronchipage = () => {
     return ( 
         <div className="bg-black flex-col flex  md:flex-row justify-between items-center md:h-screen  h-full py-5" >
             <div className=" px-20 flex flex-col justify-evenly">
-            <h1 className="text-green-500 text-3xl md:text-5xl m-8 font-bold ">Lung Cancer and Bronchi</h1>
+            <h1 className="text-blue-400  text-3xl md:text-5xl m-8 font-bold ">Lung Cancer and Bronchi</h1>
             <p className="text-gray-400 text-l  ">Certainly! Lung cancer can affect the bronchi, which are part of the respiratory system. </p>
         <p className="text-gray-400 text-l  "> There are two main types: non-small cell lung cancer (NSCLC) and small cell lung cancer (SCLC).</p>
         <p className="text-gray-400 text-l  ">NSCLC types like adenocarcinoma and squamous cell carcinoma can occur in the bronchi. SCLC often starts in the central bronchi.</p>
