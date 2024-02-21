59c9a687a5531f7dcec15cd53b4fe8612b2ae71d
Maahi Pal <maahipal94@gmail.com>
2024-02-21 20:48:44 +0530

    commit

--- src/Components/Bronchipage.jsx
+++ src/Components/Bronchipage.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Bronchipage = () => {
     return ( 
-        <div className="bg-black flex  md:flex-row justify-between items-center md:h-screen  py-5" >
+        <div className="bg-black flex-col flex  md:flex-row justify-between items-center md:h-screen  h-full py-5" >
             <div className=" px-20 flex flex-col justify-evenly">
             <h1 className="text-green-500 text-3xl md:text-5xl m-8 font-bold ">Lung Cancer and Bronchi</h1>
             <p className="text-gray-400 text-l  ">Certainly! Lung cancer can affect the bronchi, which are part of the respiratory system. </p>
