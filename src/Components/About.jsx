a35476989bcb75ba7d99c2456ebd32a75563c82c
Maahi Pal <maahipal94@gmail.com>
2024-02-21 19:53:56 +0530

    about section

---
+++ src/Components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const About = () => {
+    return ( 
+        <div className="about">
+
+<div className=" flex-col flex  md:flex-row justify-between items-center h-full py-5 ">
+      
+      <img src="./images/human.jpg" className="w-1/1 h-1/1  " />
+      
+        <h1 className="text-black text-center text-4xl font-medium py-7 font-sans">More about Lung Cancer</h1>
+       </div>
+       </div>
+        
+     );
+}
+ 
+export default About;
