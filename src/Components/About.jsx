e871a4e2f1b2505bbf7e09716aea2843f8bc7355
Maahi Pal <maahipal94@gmail.com>
2024-02-21 19:56:01 +0530

    causes and risk factors added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,6 +9,16 @@ const About = () => {
       
         <h1 className="text-black text-center text-4xl font-medium py-7 font-sans">More about Lung Cancer</h1>
        </div>
+       <div className="para">
+        <p className="text-black  md:text-base py-3 font-sans ">
+            Causes and Risk Factors:
+            <br />
+            - Highlight common causes such as smoking, exposure to carcinogens, and genetic factors.
+            <br />
+            - Discuss risk factors that increase the likelihood of developing lung cancer.
+          </p>
+
+       </div>
        </div>
         
      );
